39474be2e804daff5ddcb2927dfb8ee07af716f0
João Victor Rondon Vincensi <105375360+JvRondon@users.noreply.github.com>
2022-09-06 15:27:30 -0400

    Add files via upload

---
+++ Produto/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PRODUTO {
+    class Program {
+        static void Main(string[] args) {
+
+            int X, Y, Prod;
+            
+            X = int.Parse(Console.ReadLine());
+            Y = int.Parse(Console.ReadLine());
+            Prod = X * Y;
+
+            Console.WriteLine("Produto é = " + Prod);
+        
+        }
+    }
+}
