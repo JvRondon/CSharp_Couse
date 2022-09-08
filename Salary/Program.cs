8cf3154b8300ea6188d07994cc30924c5e563c80
João Victor Rondon Vincensi <105375360+JvRondon@users.noreply.github.com>
2022-09-08 13:53:19 -0400

    Add files via upload

---
+++ Salary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int numero, horas;
+            double valorHora, salario;
+
+            numero = int.Parse(Console.ReadLine());
+            horas = int.Parse(Console.ReadLine());
+            valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salario = horas * valorHora;
+
+            Console.WriteLine("NUMBER = " + numero);
+            Console.WriteLine("SALARY = " + salario.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
