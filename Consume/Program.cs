8982d74d6aad69c10a353a53ad2438797a99a579
João Victor Rondon Vincensi <105375360+JvRondon@users.noreply.github.com>
2022-09-08 14:17:39 -0400

    Add files via upload

---
+++ Consume/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Consume
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            double y, kml;
+
+            x = int.Parse(Console.ReadLine());
+            y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            kml = (double)x / y;
+
+            Console.WriteLine(kml.ToString("F3", CultureInfo.InvariantCulture) + "KM/L");
+        }
+    }
+}
