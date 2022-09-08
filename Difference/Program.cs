7e739deac674ab5ee7e2ddb67221a01a16b527b7
João Victor Rondon Vincensi <105375360+JvRondon@users.noreply.github.com>
2022-09-08 13:44:37 -0400

    Add files via upload

---
+++ Difference/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Difference
+{
+
+    static void Main(string[] args)
+    {
+
+        int A, B, C, D, Tt;
+
+        A = int.Parse(Console.ReadLine());
+        B = int.Parse(Console.ReadLine());
+        C = int.Parse(Console.ReadLine());
+        D = int.Parse(Console.ReadLine());
+        Tt = (A * B - C * D);
+
+        Console.WriteLine(Tt);
+
+    }
+
+}
