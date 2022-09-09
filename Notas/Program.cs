69d5fbbbd224b2e9ee97e7fb23a08049850e8d87
João Victor Rondon Vincensi <105375360+JvRondon@users.noreply.github.com>
2022-09-09 08:29:41 -0400

    Add files via upload

---
+++ Notas/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Notas {
+    class Program{
+        static void Main(string[] args){
+            int N, quociente, resto, notas ;
+
+            N = int.Parse(Console.ReadLine());
+            Console.WriteLine(N);
+
+            resto = N;
+
+            notas = 100;
+            quociente = resto / notas;
+            Console.WriteLine(quociente + " nota(s) de R$ " + notas + ",00");
+            resto = resto % notas;
+
+            notas = 50;
+            quociente = resto / notas;
+            Console.WriteLine(quociente + " nota(s) de R$ " + notas + ",00");
+            resto = resto % notas;
+
+            notas = 20;
+            quociente = resto / notas;
+            Console.WriteLine(quociente + " nota(s) de R$ " + notas + ",00");
+            resto = resto % notas;
+
+            notas = 10;
+            quociente = resto / notas;
+            Console.WriteLine(quociente + " nota(s) de R$ " + notas + ",00");
+            resto = resto % notas;
+
+            notas = 5;
+            quociente = resto / notas;
+            Console.WriteLine(quociente + " nota(s) de R$ " + notas + ",00");
+            resto = resto % notas;
+
+            notas = 2;
+            quociente = resto / notas;
+            Console.WriteLine(quociente + " nota(s) de R$ " + notas + ",00");
+            resto = resto % notas;
+
+            Console.WriteLine(resto + " nota(s) de R$1,00");
+        }
+    }
+}
