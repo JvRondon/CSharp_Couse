daf559c8a08040f985a435edabbb59286f6feec8
João Victor Rondon Vincensi <105375360+JvRondon@users.noreply.github.com>
2022-09-09 08:41:29 -0400

    Add files via upload

---
+++ Age/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Age{
+    class Program{
+        static void Main(string[] args){
+            int N, anos, meses, dias, resto;
+
+            N = int.Parse(Console.ReadLine());
+
+            anos = N / 365;
+            resto = N % 365;
+            meses = resto / 30;
+            dias = resto % 30;
+
+            Console.WriteLine(anos + " anos(s)");
+            Console.WriteLine(anos + " mes(s)");
+            Console.WriteLine(anos + " dia(s)");
+
+        }
+    }
+}
